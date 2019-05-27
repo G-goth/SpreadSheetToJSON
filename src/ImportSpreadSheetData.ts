0088725b0bfafff1c39d94f8429f03649f467579
G-goth <gf1010redgreen@gmail.com>
2019-05-27 17:08:09 +0900

    JSONファイルでのマスタデータの個別出力と全体出力に対応

--- src/ImportSpreadSheetData.ts
+++ src/ImportSpreadSheetData.ts
@@ -1,160 +1,38 @@
 // アクティブになっているスプレッドシートのデータを取得
-function ImportMasterDataSheetActive(googleSheet: GoogleAppsScript.Spreadsheet.Sheet) {
+// tslint:disable-next-line:max-line-length
+function ImportMasterDataSheet(googleSheet: GoogleAppsScript.Spreadsheet.Sheet, sheetName?: string) {
     // シートの最終行番号、最終列番号を取得
     const startRow = 3;
     const startCol = 1;
-    const activeSheetName = SpreadsheetApp.getActiveSheet().getName();
     const lastRow = googleSheet.getLastRow();
     const lastCol = googleSheet.getLastColumn();
     const sheetData = googleSheet.getSheetValues(startRow, startCol, lastRow, lastCol);
     const sheetColumnData = googleSheet.getSheetValues(startRow, startCol, startRow, lastCol);
 
     let container = {};
-    let goldfishMasterArray: any[] = [];
-    let counter: number = 0;
-    // 基本ゲームデータマスタデータシート
-    // 列の動き
-    for(let i = 0; i < (sheetData.length - startRow); ++i){
-        // 行の動き
-        for(let j = 0; j < sheetData[0].length; ++j){
+    const goldfishMasterArray: any[] = [];
+    for (let i = 0; i < (sheetData.length - startRow); ++i) {
+        for (let j = 0; j < sheetData[0].length; ++j) {
             container[sheetColumnData[0][j].toString()] = sheetData[i + 1][j];
         }
         goldfishMasterArray[i] = container;
         container = {};
     }
-    let result = {[activeSheetName]: goldfishMasterArray};
-    Browser.msgBox(JSON.stringify(result));
+    const result = {[sheetName]: goldfishMasterArray};
+    return JSON.stringify(result);
 }
-
-// すべてのスプレッドシートのデータを取得
+// アクティブになっているスプレッドシートのデータを取得してJSON用に整形した文字列を受け取る
+function ImportMasterDataSheetActive(googleSheet: GoogleAppsScript.Spreadsheet.Sheet) {
+    const sheetName = googleSheet.getSheetName();
+    const jsonStr = ImportMasterDataSheet(googleSheet, sheetName);
+    MakeJsonFileSequentially(jsonStr, googleSheet.getSheetName());
+}
+// すべてのスプレッドシートのデータを取得してJSON用に整形した文字列を受け取る
 function ImportMasterDataAllSheets(googleSheet: GoogleAppsScript.Spreadsheet.Sheet[]) {
-    const startRow = 3;
-    const startCol = 1;
-    const sheetNames = GetMasterDataAllSheetNames();
-
-    // 基本ゲームデータマスタデータ系
-    const baseGameDataGroup: any[] = [];
-    // 金魚マスタデータ系
-    const kingyo: any[] = [];
-    let inKingyoDataGroup: any;
-    // 確率マスタデータ系
-    const probability: any[] = [];
-    let inProbabilityDataGroup: any;
-    // 得点マスタデータ系
-    const score: any[] = [];
-    let inScoreDataGroup: any;
-    // ポイゲージ関連マスタデータ系
-    const poiGauge: any[] = [];
-    let inPoiGaugeDataGroup: any;
-    // 祭りタイムマスタデータ系
-    const festival: any[] = [];
-    let inFestivalDataGroup: any;
-    // ギフト関連マスタデータ系
-    const gift: any[] = [];
-    let inGiftDataGroup: any;
-
-    // 金魚すくいゲームマスターデータの呼び出し
-    let base_game_data: any[] = [];
+    // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < googleSheet.length; ++i) {
-        const lastRow = googleSheet[i].getLastRow();
-        const lastCol = googleSheet[i].getLastColumn();
-        const sheetData = googleSheet[i].getSheetValues(startRow, startCol, lastRow, lastCol);
-        const sheetColumnData = googleSheet[i].getSheetValues(startRow, startCol, startRow, lastCol);
-
-        let container = {};
-        // 基本ゲームデータマスタデータシート
-        if (googleSheet[i].getSheetName() === sheetNames[1]) {
-            for(let i = 0; i < (lastRow - startRow); ++i){
-                for(let j = 0; j < sheetData[0].length; ++j){
-                    container[sheetColumnData[0][j].toString()] = sheetData[i + 1][j];
-                }
-                base_game_data[i] = container;
-            }
-        }
-
-        // if (googleSheet[i].getSheetName() === sheetNames[0]) {
-        //     for (let sheetDataCount = 1; sheetDataCount <= (lastRow - startRow); ++sheetDataCount) {
-        //         baseGameDataGroup[sheetDataCount - 1] = {
-        //             base_game_data_id: sheetData[sheetDataCount][0],
-        //             play_time: sheetData[sheetDataCount][1],
-        //             fever_time: sheetData[sheetDataCount][2],
-        //         };
-        //     }
-        // }
-        
-    //     // 金魚マスタデータシート
-    //     if (googleSheet[i].getSheetName() === sheetNames[1]) {
-    //         for (let sheetDataCount = 1; sheetDataCount <= (lastRow - startRow); ++sheetDataCount) {
-    //             inKingyoDataGroup = {
-    //                 kingyo_id: sheetData[sheetDataCount][0],
-    //                 probability_id: sheetData[sheetDataCount][2],
-    //                 score_id: sheetData[sheetDataCount][3],
-    //             };
-    //             kingyo[sheetDataCount - 1] = inKingyoDataGroup;
-    //         }
-    //     }
-    //     // 確率マスタデータシート
-    //     if (googleSheet[i].getSheetName() === sheetNames[2]) {
-    //         for (let sheetDataCount = 1; sheetDataCount <= (lastRow - startRow); ++sheetDataCount) {
-    //             inProbabilityDataGroup = {
-    //                 probability_id: sheetData[sheetDataCount][0],
-    //                 kingyo_id: sheetData[sheetDataCount][1],
-    //                 occurrence_probability: sheetData[sheetDataCount][2],
-    //             };
-    //             probability[sheetDataCount - 1] = inProbabilityDataGroup;
-    //         }
-    //     }
-    //     // 得点マスタデータシート
-    //     if (googleSheet[i].getSheetName() === sheetNames[3]) {
-    //         for (let sheetDataCount = 1; sheetDataCount <= (lastRow - startRow); ++sheetDataCount) {
-    //             inScoreDataGroup = {
-    //                 score_id: sheetData[sheetDataCount][0],
-    //                 kingyo_id: sheetData[sheetDataCount][1],
-    //                 score: sheetData[sheetDataCount][2],
-    //                 festival_point: sheetData[sheetDataCount][3],
-    //             };
-    //             score[sheetDataCount - 1] = inScoreDataGroup;
-    //         }
-    //     }
-    //     // ポイゲージマスタデータシート
-    //     if (googleSheet[i].getSheetName() === sheetNames[4]) {
-    //         for (let sheetDataCount = 1; sheetDataCount <= (lastRow - startRow); ++sheetDataCount) {
-    //             inPoiGaugeDataGroup = {
-    //                 poigauge_id: sheetData[sheetDataCount][0],
-    //                 max_gauge_id: sheetData[sheetDataCount][1],
-    //                 min_gauge_id: sheetData[sheetDataCount][2],
-    //                 decrease_point: sheetData[sheetDataCount][3],
-    //             };
-    //             poiGauge[sheetDataCount - 1] = inPoiGaugeDataGroup;
-    //         }
-    //     }
-    //     // 祭りタイムマスタデータシート
-    //     if (googleSheet[i].getSheetName() === sheetNames[5]) {
-    //         for (let sheetDataCount = 1; sheetDataCount <= (lastRow - startRow); ++sheetDataCount) {
-    //             inFestivalDataGroup = {
-    //                 festival_time_id: sheetData[sheetDataCount][0],
-    //                 festival_time_count: sheetData[sheetDataCount][1],
-    //                 up_to_wasshoi_count: sheetData[sheetDataCount][2],
-    //                 in_wasshoi_festival_point: sheetData[sheetDataCount][3],
-    //                 festival_coeff: sheetData[sheetDataCount][4],
-    //             };
-    //             festival[sheetDataCount - 1] = inFestivalDataGroup;
-    //         }
-    //     }
-    //     // ギフトマスタデータシート
-    //     if (googleSheet[i].getSheetName() === sheetNames[6]) {
-    //         for (let sheetDataCount = 1; sheetDataCount <= (lastRow - startRow); ++sheetDataCount) {
-    //             inGiftDataGroup = {
-    //                 gift_point_id: sheetData[sheetDataCount][0],
-    //                 gift_name: sheetData[sheetDataCount][1],
-    //                 festival_point: sheetData[sheetDataCount][2],
-    //             };
-    //             gift[sheetDataCount - 1] = inGiftDataGroup;
-    //         }
-    //     }
-    // }
+        const sheetName = googleSheet[i].getSheetName();
+        const jsonStr = ImportMasterDataSheet(googleSheet[i], sheetName);
+        MakeJsonFileSequentially(jsonStr, googleSheet[i].getSheetName());
     }
-    const result = {[sheetNames[0]]: base_game_data};
-    Browser.msgBox(JSON.stringify(result));
-    // MakeJsonFile(JSON.stringify(result));
 }
--- src/MakeJsonFile.ts
+++ src/MakeJsonFile.ts
@@ -1,29 +1,50 @@
 // 受け取った文字列をJSONファイルを作る
 function MakeJsonFile(jsonStr: any) {
-    const contentType = "text/plain";
+    /*const contentType = "text/plain";
     const charSet = "UTF-8";
     const lineDelimiter = ",";
-    const newLineChar = "\n";
+    const newLineChar = "\n";*/
     const fileName = "GoldFishCaptureMasterData.json";　// 任意のjsonのファイル名を記入
     const folderId = "1D0s0eM3jDNW6NRyk3Qcfbe-dnxLbXEeA";
 
     // 既に同盟のファイルがある場合は削除する
     const folder = DriveApp.getFolderById(folderId);
     if (folder.getFilesByName(fileName).hasNext()) {
-        let opt = Browser.msgBox("既にマスタデータファイルがあります。生成してもよろしいですか？", Browser.Buttons.OK_CANCEL);
+        const opt = Browser.msgBox("既にマスタデータファイルがあります。生成してもよろしいですか？", Browser.Buttons.OK_CANCEL);
         if (opt === "ok") {
             // ファイルの削除
-            let jsonFile = folder.getFilesByName(fileName).next();
+            const jsonFile = folder.getFilesByName(fileName).next();
             folder.removeFile(jsonFile);
             // ファイルの作成
             folder.createFile(fileName, jsonStr);
-        }
-        else {
+        } else {
             Browser.msgBox("ファイルの生成を取りやめました。");
         }
-    }
-    else {
+    } else {
         // ファイルの新規作成
         folder.createFile(fileName, jsonStr);
+    }
+}
+
+// 受け取った文字列をJSONファイルを作る(すべて)
+function MakeJsonFileSequentially(jsonStr: any, fileName: string) {
+    /*const contentType = "text/plain";
+    const charSet = "UTF-8";
+    const lineDelimiter = ",";
+    const newLineChar = "\n";*/
+    const fullFileName = fileName + ".json";
+    const folderId = "1D0s0eM3jDNW6NRyk3Qcfbe-dnxLbXEeA";
+
+    // 既に同盟のファイルがある場合は削除する
+    const folder = DriveApp.getFolderById(folderId);
+    if (folder.getFilesByName(fullFileName).hasNext()) {
+        // ファイルの削除
+        const jsonFile = folder.getFilesByName(fullFileName).next();
+        folder.removeFile(jsonFile);
+        // ファイルの作成
+        folder.createFile(fullFileName, jsonStr);
+    } else {
+        // ファイルの新規作成
+        folder.createFile(fullFileName, jsonStr);
     }
 }
