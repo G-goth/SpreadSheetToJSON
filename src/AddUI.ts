db64101794e6f143b13e43a36eebd32c6a9894ee
G-goth <gf1010redgreen@gmail.com>
2019-05-29 17:52:16 +0900

    メニュー文言の修正

--- src/AddUI.ts
+++ src/AddUI.ts
@@ -7,7 +7,7 @@ function onOpen() {
     const ui = SpreadsheetApp.getUi();
     const menu = ui.createMenu("マスタデータ");
     // menu.addItem("現在のシートをJSONに出力", methodNames[0]);
-    menu.addItem("すべてのシートをJSONに出力(1ファイル)", methodNames[1]);
+    menu.addItem("すべてのシートをJSONに出力", methodNames[1]);
     // menu.addItem("すべてのシートをJSONに出力(個別ファイル)", methodNames[2]);
     menu.addToUi();
 }
