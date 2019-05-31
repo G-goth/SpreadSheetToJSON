564c0050efaec55ea446991854fefe6bb2ff0285
G-goth <gf1010redgreen@gmail.com>
2019-05-31 12:40:11 +0900

    ファイル生成時の文言の調整とリンク表示をするコードの削除

--- src/ImportSpreadSheetData.ts
+++ src/ImportSpreadSheetData.ts
@@ -4,18 +4,21 @@ class GetSpreadSheetData {
     private startCol: number = 1;
 
     // すべてのシートのデータをJSON文字列として出力
-    public OutPutMasterDataToJSON(googleSheet: GoogleAppsScript.Spreadsheet.Sheet[]): string {
+    public OutPutMasterDataToJSON(googleSheet: GoogleAppsScript.Spreadsheet.Sheet[]): void {
+        const makeJson: MakeJsonFiles = new MakeJsonFiles();
         let result: string = "";
         for (let i = 0; i < (googleSheet.length - 1); ++i) {
             result += this.ImportMasterDataSheet(googleSheet[i], googleSheet[i].getSheetName()) + ",";
         }
         result += this.ImportMasterDataSheet(googleSheet[googleSheet.length - 1], googleSheet[googleSheet.length - 1].getSheetName());
-        Browser.msgBox("{" + result + "}");
-        return "";
+        makeJson.MakeJsonFile("{" + result + "}");
+        // Browser.msgBox("{" + result + "}");
     }
-    public OutPutActiveMasterDataToJSON(googleSheet: GoogleAppsScript.Spreadsheet.Sheet) {
+    public OutPutActiveMasterDataToJSON(googleSheet: GoogleAppsScript.Spreadsheet.Sheet): void {
+        const makeJson: MakeJsonFiles = new MakeJsonFiles();
         const result: string = this.ImportMasterDataSheet(googleSheet, googleSheet.getSheetName());
-        Browser.msgBox("{" + result + "}");
+        makeJson.MakeJsonFile("{" + result + "}");
+        // Browser.msgBox("{" + result + "}");
     }
 
     private ImportMasterDataSheet(googleSheet: GoogleAppsScript.Spreadsheet.Sheet, sheetName: string): string {
@@ -58,64 +61,4 @@ class GetSpreadSheetData {
         const result = JSON.stringify(goldfishMasterArray);
         return result;
     }
-}
-
-function ImportMasterDataSheet(googleSheet: GoogleAppsScript.Spreadsheet.Sheet, sheetName?: string) {
-    // シートの最終行番号、最終列番号を取得
-    const startRow = 3;
-    const startCol = 1;
-    const lastRow = googleSheet.getLastRow();
-    const lastCol = googleSheet.getLastColumn();
-    const sheetData = googleSheet.getSheetValues(startRow, startCol, lastRow, lastCol);
-    const sheetColumnData = googleSheet.getSheetValues(startRow, startCol, startRow, lastCol);
-
-    const goldfishMasterArray: any[] = [];
-    const columnDiffCount: number = sheetData.length - startRow;
-
-    let container = {};
-    let result: string = "";
-    // マスターのカラムが1つしかない場合
-    if (columnDiffCount <= 1) {
-        for (let i = 0; i < sheetData[0].length; ++i) {
-            container[sheetColumnData[0][i].toString()] = sheetData[1][i];
-        }
-        result = JSON.stringify(container);
-    }
-    // マスターのカラムが2つ以上ある場合
-    else {
-        for (let i = 0; i < (sheetData.length - startRow); ++i) {
-            for (let j = 0; j < sheetData[0].length; ++j) {
-                container[sheetColumnData[0][j].toString()] = sheetData[i + 1][j];
-            }
-            goldfishMasterArray[i] = container;
-            container = {};
-        }
-        result = JSON.stringify(goldfishMasterArray);
-    }
-    return '"' + sheetName + '":' + result;
-}
-// アクティブになっているスプレッドシートのデータを取得してJSON用に整形した文字列を受け取る
-function ImportMasterDataSheetActive(googleSheet: GoogleAppsScript.Spreadsheet.Sheet) {
-    const sheetName = googleSheet.getSheetName();
-    const jsonStr = ImportMasterDataSheet(googleSheet, sheetName);
-    // Browser.msgBox("{" + jsonStr + "}");
-    MakeJsonFile(("{" + jsonStr + "}"));
-}
-// すべてのスプレッドシートのデータを取得してJSON用に整形した文字列を受け取る
-function ImportMasterDataSheetAllinOne(googleSheet: GoogleAppsScript.Spreadsheet.Sheet[]) {
-    let joinedJsonStr: string = "";
-    const jsonStrArray: any[] = [];
-    const fileNames: string[] = [];
-    for (let i = 0; i < googleSheet.length; ++i) {
-        fileNames[i] = googleSheet[i].getSheetName();
-        jsonStrArray[i] = ImportMasterDataSheet(googleSheet[i], fileNames[i]);
-    }
-
-    // JSONにシリアライズされた文字列同士を結合
-    for (let i = 0; i < (jsonStrArray.length - 1); ++i) {
-        joinedJsonStr += jsonStrArray[i] + ",";
-    }
-    joinedJsonStr += jsonStrArray[jsonStrArray.length - 1];
-    // Browser.msgBox("{" + joinedJsonStr + "}");
-    MakeJsonFile("{" + joinedJsonStr + "}");
 }
--- src/MakeJsonFile.ts
+++ src/MakeJsonFile.ts
@@ -3,11 +3,10 @@ class MakeJsonFiles {
     private fileName: string = "GoldFishCaptureMasterData.json";　// 任意のjsonのファイル名を記入
     private folderId: string = "1pbBwWgWKsEZde6q75q1jQI4kK1FX9pQx";
     private folder: GoogleAppsScript.Drive.Folder = DriveApp.getFolderById(this.folderId);
+
     public MakeJsonFile(jsonStr: string): void {
-        /*const fileName: string = "GoldFishCaptureMasterData.json";　// 任意のjsonのファイル名を記入
-        const folderId: string = "1pbBwWgWKsEZde6q75q1jQI4kK1FX9pQx";
-        const folder: GoogleAppsScript.Drive.Folder = DriveApp.getFolderById(folderId);*/
-        
+        const addUi: FileTrasporter = new FileTrasporter(this.folderId);
+
         if (this.folder.getFilesByName(this.fileName).hasNext()) {
             // 同名ファイルがあった場合は削除
             this.MakeNewJsonFile(jsonStr);
@@ -15,53 +14,23 @@ class MakeJsonFiles {
         else {
             // ファイルの新規作成
             this.folder.createFile(this.fileName, jsonStr);
-            GenerateFilesLink(this.folderId);
+            addUi.GeneretaFolderPathLink();
         }
     }
 
     private MakeNewJsonFile(jsonStr: string): void {
-        const opt = Browser.msgBox("", Browser.Buttons.OK_CANCEL);
+        const addUi: FileTrasporter = new FileTrasporter(this.folderId);
+        const opt = Browser.msgBox("同名のマスタデータファイルが既にあります。上書き保存しますか？", Browser.Buttons.OK_CANCEL);
         if (opt === "ok") {
             // ファイルの削除
             const jsonFile = this.folder.getFilesByName(this.fileName).next();
             this.folder.removeFile(jsonFile);
             // ファイルの作成
             this.folder.createFile(this.fileName, jsonStr);
-            GenerateFilesLink(this.folderId);
+            addUi.GeneretaFolderPathLink();
         }
         else {
             Browser.msgBox("ファイルの生成を取りやめました。");
         }
-    }
-}
-
-function MakeJsonFile(jsonStr: string) {
-    /*const contentType = "text/plain";
-    const charSet = "UTF-8";
-    const lineDelimiter = ",";
-    const newLineChar = "\n";*/
-    const fileName = "GoldFishCaptureMasterData.json";　// 任意のjsonのファイル名を記入
-    const folderId = "1pbBwWgWKsEZde6q75q1jQI4kK1FX9pQx";
-
-    // 既に同名のファイルがある場合は削除する
-    const folder = DriveApp.getFolderById(folderId);
-    if (folder.getFilesByName(fileName).hasNext()) {
-        const opt = Browser.msgBox("既にマスタデータファイルがあります。生成してもよろしいですか？", Browser.Buttons.OK_CANCEL);
-        if (opt === "ok") {
-            // ファイルの削除
-            const jsonFile = folder.getFilesByName(fileName).next();
-            folder.removeFile(jsonFile);
-            // ファイルの作成
-            folder.createFile(fileName, jsonStr);
-            GenerateFilesLink(folderId);
-        }
-        else {
-            Browser.msgBox("ファイルの生成を取りやめました。");
-        }
-    }
-    else {
-        // ファイルの新規作成
-        folder.createFile(fileName, jsonStr);
-        GenerateFilesLink(folderId);
     }
 }
