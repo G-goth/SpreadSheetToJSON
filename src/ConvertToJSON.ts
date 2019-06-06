c1e7eb5a539778f277bdeb9e7af5d1c98d5a5b93
G-goth <gf1010redgreen@gmail.com>
2019-06-06 19:42:34 +0900

    メソッドの型を明示的に

--- src/ConvertToJSON.ts
+++ src/ConvertToJSON.ts
@@ -1,12 +1,12 @@
 class DownloadJSONFile {
-    public DownloadJsonFileActiveSheet() {
+    public DownloadJsonFileActiveSheet(): void {
         const getData = new GetSpreadSheetData();
         const activeSheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
         // getData.OutPutActiveMasterDataToJSON(activeSheet);
     }
     
     // 開いているスプレッドシートのすべてのシートをJSONファイルに出力する(個別ファイルに出力)
-    public DownloadJsonFileSheetAllinOne() {
+    public DownloadJsonFileSheetAllinOne(): void {
         const getData = new GetSpreadSheetData();
         const allInOneSheets = SpreadsheetApp.getActiveSpreadsheet().getSheets();
         getData.OutPutMasterDataToJSON(allInOneSheets);
